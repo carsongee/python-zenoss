a30a7373263fe9d7d512be45a7008cb91970e530
Carson Gee <x@carsongee.com>
2013-11-27 13:21:18 -0500

    Added quick script for adding a device.

---
+++ scripts/zenoss_add_device.py
@@ -0,0 +1,53 @@
+#!/bin/env python
+
+import argparse
+import os
+import zenossapi
+
+def add_device(host, username, password, device, device_class, pem=None):
+    """
+    Adds the device using to the specified class using provided credentials.
+    """
+    z = zenossapi.ZenossAPI()
+    try:
+        z.connect(host, username, password, pem)
+    except Exception, ex:
+        print(str(ex))
+        return -1
+
+    try:
+        z.add_device(device, device_class)
+    except Exception, ex:
+        print(str(ex))
+        return -1
+    return 0
+
+if __name__ == "__main__":
+    """Handle argument passing from command line"""
+
+    parser = argparse.ArgumentParser(prog='zenoss_add_device.py')
+
+    parser.add_argument('host', help='Zenoss host')
+    parser.add_argument('username', help='Zenoss username')
+    parser.add_argument('password', help='Zenoss password')
+    parser.add_argument('-K', '--client-pem', type=str, help='Zenoss user pem file')
+    parser.add_argument('device', help='Device to add')
+    parser.add_argument('device_class', help='Device class for new device')
+
+    args = parser.parse_args()
+
+    if args.client_pem:
+        if not os.path.exists(args.client_pem):
+            print('Specified client pem file doesn\'t exist\n')
+            sys.exit(1)
+
+    sys.exit(
+        add_device(
+            args.host,
+            args.username,
+            args.password,
+            args.device,
+            args.device_class,
+            args.client_pem
+            )
+        )
